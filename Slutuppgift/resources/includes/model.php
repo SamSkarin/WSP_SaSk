1de9edc7825618fc9739c2c977384160ffd3793c
SamSkarin <32635902+SamSkarin@users.noreply.github.com>
2018-05-28 09:59:51 +0200

    Update 28/05/18
    Slutuppgift

---
+++ Slutuppgift/resources/includes/model.php
@@ -0,0 +1,70 @@
+<?php
+require 'resources/includes/db_conn.php'; // Menas att dem t.ex. behöver uppkoppling från PHPmyAdmin.
+
+if ($pdo) {
+
+    // Visar en lista med alla inlägg som gjorts, och vilka som har gjort dem.
+    $sql = 'SELECT P.ID, P.Slug, P.Headline, CONCAT(U.Firstname, " ", U.Lastname) AS Name, P.Creation_time, P.Text FROM Post AS P JOIN Users AS U ON U.ID = P.User_ID ORDER BY P.Creation_time DESC';
+
+    // Representerar sökrutan.
+    if (isset($_POST['search'])) {
+        // Och sedan visar resultatet genom What.
+        $data = $_POST['what'];
+
+        /**********************************************************/
+        /*********************** C-UPPGIFT 1 **********************/
+        /*** Variabeln $data kan innehålla, som den är utformad, **/
+        /********* information som kan skada vår databas. *********/
+        /*** För betyget C så kräver jag att ni säkerställer att **/
+        /***** $data inte innehåller någon form av skadlig kod ****/
+        /**********************************************************/
+
+        /**********************************************************/
+        /*********************** C-UPPGIFT 2 **********************/
+        /* I filen all-posts.php så skrivs det ut en kortare text */
+        /* följt av en länk till berört inlägg. Vore det inte mer */
+        /* passande att det istället skrivs ut ord från inlägget? */
+        /* För betyget C så kräver jag att ni tar fram en lösning */
+        /**** där 10 ord från inlägget skrivs ut före läs mer. ****/
+        /************ Tänk implode och/eller explode! *************/
+        /**********************************************************/
+
+        /**********************************************************/
+        /************************ A-UPPGIFT ***********************/
+        /** Som det är just nu så tar vi bara in en variabel som **/
+        /******* vi använder när vi söker igenom databasen. *******/
+        /* Tittar man på sidor som exempelvis google och facebook */
+        /**** så kan din sökning oftast innehålla flera sökord ****/
+        /* För betyget A så kräver jag att ni tar fram en lösning */
+        /** som gör det möjligt för användaren att kunna söka på **/
+        /** flera separerade ord. Att man exempelvis kan söka på **/
+        /***** texter som innehåller både "Lorum" och "Ipsum." ****/
+        /**********************************************************/
+
+        // Om resultatet inte är tomt, så kommer den att ge en lista inlägg som skapats.
+        if (!empty($data)) {
+            $sql = 'SELECT p.ID, p.Slug, p.Headline, CONCAT(u.Firstname, " ", u.Lastname) AS Name, p.Creation_time, p.Text FROM Posts AS p JOIN Users AS u ON U.ID = P.User_ID WHERE p.Text LIKE "%'.$data.'%" ORDER BY P.Creation_time DESC';
+        }
+    }
+
+    // Skapar en array
+    $model = array();
+
+    // Sorterar resultat från PDO till Row.
+    foreach($pdo->query($sql) as $row) {
+        // Strukturerar varje rad från foreach i en array.
+        $model += array(
+            $row['ID'] => array(
+                'slug' => $row['Slug'],
+                'title' => $row['Headline'],
+                'author' => $row['Name'],
+                'date' => $row['Creation_time'],
+                'text' => $row['Text']
+            )
+        );
+    }
+}
+else {
+    print_r($pdo->errorInfo());
+}
+?>
